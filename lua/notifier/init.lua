a2fa71e97fb6f56381787e57796a8cf79cea0cea
Sam Sze <mingsum.sam@gmail.com>
2024-05-15 20:43:30 +0800

    fix: fix max width and height not integer value

--- lua/notifier/init.lua
+++ lua/notifier/init.lua
@@ -106,8 +106,8 @@ vim.notify = function(msg, level) ---@diagnostic disable-line: duplicate-set-fie
 
     popup:update_layout({
       size = {
-        width = math.min(vim.o.columns * 0.3, cols),
-        height = math.min(vim.o.lines * 0.8, #lines),
+        width = math.min(math.floor(vim.o.columns * 0.3), cols),
+        height = math.min(math.floor(vim.o.lines * 0.8), #lines),
       },
     })
     popup:show()
